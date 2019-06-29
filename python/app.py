0826c3dfc1e54fbf212701450134ddf0277231c2
Licco <liccowee@gmail.com>
2019-06-29 14:04:25 +0800

    serialize object to json

--- python/app.py
+++ python/app.py
@@ -4,6 +4,10 @@ from bson import json_util
 import json
 app = Flask(__name__)
 
+class User:
+    name = ""
+    phone = ""
+    id = ""
 
 @app.route('/example')
 def hello():
@@ -14,8 +18,13 @@ def checkDatabase():
     client = MongoClient('mongo', 27017)
     db = client.data
     collection = db.user
+    findUser = collection.find_one()
+    user = User()
+    user.name = findUser['name']
+    user.phone = findUser['phone']
+    user.id = str(findUser['_id'])
     json_projects = []
-    json_projects = json.dumps(collection.find_one(), default=json_util.default)
+    json_projects = json.dumps(user.__dict__, default=lambda o: o.__dict__, indent=4)
     client.close()
     return json_projects
 
