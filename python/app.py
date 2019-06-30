3c7fc290e10c41b625c794c5ebd7823555b796f7
Licco <liccowee@gmail.com>
2019-06-30 23:59:40 +0800

    added require entry point for [Didi] to complete

--- python/app.py
+++ python/app.py
@@ -13,10 +13,28 @@ class Response:
     def __init__(self, success):
         self.success = success
 
-@app.route('/example')
-def hello():
-    return "Hello World!"
+# Should be access only for ADMIN. So only admin can check in user. Need to send user id as params for check in user
+@app.route('/checkin', methods=['POST'])
+def checkin():
+    return "Checking in User"
 
+# Should be access only for ADMIN. So only admin can check out user.
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    return "Checking out User"
+
+# Should be return all the history for the requested access token
+@app.route('/history', methods=['GET'])
+def history():
+    return "Get all history session of user"
+
+# Should be return user profile if I query with access token. User profile data such as User ID
+@app.route('/profile', methods=['GET'])
+def profile():
+    return "Get user profile such as Profile ID, phone number and name"
+
+# This /register api service should be in POST method. Need to change it!
+# should also return access token so web know it is from which user
 @app.route('/register/<name>/<phone>')
 def register(name, phone):
     client = MongoClient('mongo', 27017)
@@ -30,6 +48,12 @@ def register(name, phone):
     json_projects = json.dumps(response.__dict__, default=lambda o: o.__dict__, indent=4)
     return json_projects
 
+# This /example is for sample purpose. Need to remove in future!
+@app.route('/example')
+def hello():
+    return "Hello World!"
+
+# This /db is for sample purpose. Need to remove in future!
 @app.route("/db")
 def checkDatabase():
     client = MongoClient('mongo', 27017)
